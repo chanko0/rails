12a18b4912b5df449741c28b1be84851cff665d0
EC2 Default User <ec2-user@ip-172-31-92-56.ec2.internal>
2021-08-07 12:34:01 +0000

    [Add]7章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '05414768c4d6a6dbd69397d73280da58b0559cc87c00db79510d35e0109daf78dfaf1e3555e55ca8771de5bb4ab0b1b40bad4eb66873f5a722a4ec97f78caa3d'
